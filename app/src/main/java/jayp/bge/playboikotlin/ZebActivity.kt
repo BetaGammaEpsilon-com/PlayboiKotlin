5504d605006d41504ad452edf54390e7fea724a9
zohebaziz <zaziz101@gmail.com>
2021-10-19 23:18:56 -0400

    create zeb activity

---
+++ app/src/main/java/jayp/bge/playboikotlin/ZebActivity.kt
@@ -0,0 +1,11 @@
+package jayp.bge.playboikotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ZebActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_zeb)
+    }
+}
