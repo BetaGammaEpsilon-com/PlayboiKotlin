37f64726b2b4c9aa78ebc0d7acd7a9949b4229d5
Robert Chatterton <robbie.chatterton@gmail.com>
2021-11-08 22:31:50 -0500

    week 4 -- http requests in fragment from last week

---
+++ app/src/main/java/jayp/bge/playboikotlin/robbie/fragments/webloader/EightBall.kt
@@ -0,0 +1,8 @@
+package jayp.bge.playboikotlin.robbie.fragments.webloader
+
+data class EightBall(
+    val message: String,
+    val demand: String,
+    val id: Int,
+    val liveTo: Int,
+)
--- app/src/main/java/jayp/bge/playboikotlin/robbie/fragments/webloader/RobbieWebLoaderFragment.kt
+++ app/src/main/java/jayp/bge/playboikotlin/robbie/fragments/webloader/RobbieWebLoaderFragment.kt
@@ -3,21 +3,79 @@ package jayp.bge.playboikotlin.robbie.fragments.webloader
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import jayp.bge.playboikotlin.databinding.FragmentRobbieWebLoaderBinding
 
 class RobbieWebLoaderFragment : Fragment() {
 
     private lateinit var binding: FragmentRobbieWebLoaderBinding
+    private val viewModel: RobbieWebLoaderViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         binding = FragmentRobbieWebLoaderBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setClickListeners()
+        viewModel.viewStateLiveData.observe(viewLifecycleOwner) {
+            observeViewState(it)
+        }
+    }
+
+    private fun observeViewState(viewState: RobbieWebLoaderViewState) {
+        when (viewState.webResponse) {
+            is Failure -> onWebFailure(viewState.webResponse.errorMessage)
+            Loading -> showLoading(true)
+            is Success -> onWebSuccess(viewState.webResponse.data)
+        }
+    }
+
+    private fun onWebFailure(errorMessage: String) {
+        with(binding) {
+            showLoading(false)
+            robbieWlLayout.visibility = GONE
+            robbieWlBottomText.text = errorMessage
+        }
+    }
+
+    private fun onWebSuccess(data: List<EightBall>?) {
+        with(binding) {
+            data?.let { nonNullData ->
+                robbieWlBottomText.text =
+                    nonNullData.map { it.toString() }.reduce { acc, s -> acc + "\n" + s + "\n" }
+            } ?: kotlin.run {
+                robbieWlBottomText.text = "rob response was null"
+            }
+            showLoading(false)
+            robbieWlLayout.visibility = VISIBLE
+        }
+    }
+
+    private fun showLoading(shouldShow: Boolean) {
+        with(binding.robbieWlProgressBar) {
+            if (shouldShow) {
+                visibility = VISIBLE
+                animate()
+            } else {
+                visibility = GONE
+                clearAnimation()
+            }
+        }
+    }
+
+    private fun setClickListeners() {
+        binding.webLoaderButton.setOnClickListener {
+            viewModel.sendWebRequest(1)
+        }
+    }
 }
